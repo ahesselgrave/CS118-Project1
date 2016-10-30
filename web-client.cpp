75b78896a67fdc449e07ee93d325b54575ef4c92
sabrina <sabrina>
2016-10-30 02:42:11 -0700

    Added DNS translation to client

--- web-client.cpp
+++ web-client.cpp
@@ -8,13 +8,14 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <netdb.h>
 
 #include <iostream>
 #include <sstream>
 
 int main( int argc, char *argv[] )
-{
-  vector<string> clientArgs; // hold command line arguments
+{ // hold command line arguments
+  vector<string> clientArgs;
   
   // parse command line arguments
   if (argc > 1){
@@ -24,22 +25,115 @@ int main( int argc, char *argv[] )
   }
 
   for (int j = 0; j < argc - 1; j++){
-    //iterate through each item in clientArgs
+    //iterate through each item in clientArgs 
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    // create HttpRequest
+    HttpRequest request = HttpRequest(clientArgs[j]);
+    request.parseRequestInput(clientArgs[j]);
+    request.setRequestInput();
+    request.addHeader("HOST: " + request.getHost());
+    request.addHeader("Connection: close");
+    // might need more headers
+    request.setHeaderString();
+    request.createMessageString();
+    // this is the http request to send 
+    string toSend = request.getMessageString();
+
+    // create and initilaize sockaddr_in
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    // short, network byte order
+    serverAddr.sin_port = htons(atoi(request.getPort().c_str()));
+    
+    memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+
+    // host to IP conversion
+    struct addrinfo hints;
+    struct addrinfo* res;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+
+    // get address
+    int status = 0;
+    if ((status = request.getHost(), serverAddr.sin_port, &hints, &res)) != 0) {
+      std::cerr << "getaddrinfo: " << gai_strerror(status) << std::endl;
+      return 2;
+    }
+
+    // connect to server
+    int sfd;
+    struct sockaddr_in* ipv4;
+    // iterate through linked list of getaddrinfo structures
+    for(struct addrinfo* p = res; p != 0; p = p->ai_next) {
+      // try each address until successful connection
+      sfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+      if (sfd == -1)
+	continue;
+      if (connect(sfd, p->ai_addr, p->ai_addrlen) != -1)
+	break;
+      close(sfd);
+    }
+    // error if no address worked
+    if (p == 0){
+      perror("connect");
+      return 2;
+    }
+    // save address to serverAddr
+    // there is a very good chance I got the syntax for this wrong??
+    ipv4 = (struct sockaddr_in*)p->ai_addr;
+    serverAddr.sin_addr.s_addr = ipv4->sin_addr.s_addr;
+    // free linked list
+    freeaddrinfo(res);
+    
+    //serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    //memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+    /*   
+    // connect to the server
+    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+      perror("connect");
+      return 2;
+    }
+    */
+
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
+      perror("getsockname");
+      return 3;
+    }
+
+    char ipstr[INET_ADDRSTRLEN] = {'\0'};
+    inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+    std::cout << "Set up a connection from: " << ipstr << ":" <<
+      ntohs(clientAddr.sin_port) << std::endl;
+
+    // IMPLEMENT SENDING AND RECEIVING HERE
+
+
+    close(sockfd);
+
   }
-  /*
-  implementation for later (?)
-  for each item in clientArgs:
-     - isolate web server
-        - establish connection
-        - verify connection is established
-     - construct HTTP request
-     - wait for response
-        - parse response for success/failure codes
-     - save file to current directory
-        - name based on URL ending
 
 
-   */
+
+
+  /* Previous notes by Sabrina                                                                           
+  implementation for later (?)                                                  
+  for each item in clientArgs:                                                  
+     - isolate web server                                                       
+        - establish connection                                                  
+        - verify connection is established                                      
+     - construct HTTP request                                                   
+     - wait for response                                                        
+        - parse response for success/failure codes                              
+     - save file to current directory                                           
+        - name based on URL ending                                              
+                                                                                
+                                                                                
+  */
 
 
 
